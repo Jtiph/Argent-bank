e52648ffdf3a829165b4b11ac19cc69f7733c3f0
Tiphanie Jan <jan.tiphanie@outlook.fr>
2025-02-02 10:54:11 +0100

    ajout des pages singin et user

--- ArgentBank-Backend-main/server/database/connection.js
+++ ArgentBank-Backend-main/server/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-require('dotenv').config(); 
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+
+const databaseUrl ="mongodb://Jtiph:ue8xI42NxiIS7z4Z@argentbankdb-shard-00-00.pm62q.mongodb.net:27017,argentbankdb-shard-00-01.pm62q.mongodb.net:27017,argentbankdb-shard-00-02.pm62q.mongodb.net:27017/?ssl=true&replicaSet=atlas-rioad6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=argentBankDB"
+
 
 module.exports = async () => {
   try {
@@ -12,3 +12,4 @@ module.exports = async () => {
     throw new Error(error)
   }
 }
+
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const SignIn = () => {
   return (
-    <div>
-
-    </div>
+    <div className="main bg-dark">
+      <form>
+        <div className="input-wrapper">
+          <label for="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+        <div class="input-wrapper">
+          <label for="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div class="input-remember">
+          <input type="checkbox" id="remember-me" /><label for="remember-me"
+            >Remember me</label>
+        </div>
+      </form>
+  </div>
   );
 };
 
